b34f3022a74776fb9156c6d29aaa982d415edf31
SEAN LEE <seanlee@MacBook-Pro.local>
2021-08-04 14:22:08 -0500

    Add config files.

--- db/database.js
+++ db/database.js
@@ -3,15 +3,9 @@ import { config } from "../config.js";
 
 const { user, password, database, host, port } = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
-    dialect: 'postgres',
+    dialect: 'mysql',
     host,
     port,
     logging:false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        },
-    },
 });
 
