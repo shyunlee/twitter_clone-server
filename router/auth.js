1066319ad45a382a2508dcaad0c48051532d4c71
shyunlee <shyunlee0909@gmail.com>
2022-01-06 16:43:52 -0600

    test logic completed for auth integration

--- router/auth.js
+++ router/auth.js
@@ -10,6 +10,7 @@ const validateCredential = [
     body('username')
         .trim()
         .notEmpty()
+        .isLength({min:3})
         .withMessage('username should be at least 5 characters'),
     body('password')
         .trim()
--- tests/integration-tests/auth.test.js
+++ tests/integration-tests/auth.test.js
@@ -1,77 +1,153 @@
 import { startServer, stopServer } from "../../app.js";
-import axios from 'axios'
+import axios from "axios";
 import { sequelize } from "../../db/database.js";
-import faker from 'faker'
+import faker from "faker";
 
-
-describe('Auth API Test', () => {
+describe("Auth API Test", () => {
   let server;
   let httpRequest;
   beforeAll(async () => {
-    server = await startServer()
+    server = await startServer();
     httpRequest = axios.create({
-      baseURL: 'http://localhost:8080',
-      validateStatus: null
-    })
-  })
+      baseURL: "http://localhost:8080",
+      validateStatus: null,
+    });
+  });
 
   afterAll(async () => {
-    await sequelize.drop()
-    await stopServer(server)
-  })
+    await sequelize.drop();
+    await stopServer(server);
+  });
 
-  describe('POST to /auth/signup', () => {
-    it('returns 201 and authorization token when user details are valid', async () => {
-      const user = createValidUserDetail()
-      const response = await httpRequest.post('/auth/signup', user)
-      
-      expect(response.status).toBe(201)
+  describe("POST to /auth/signup", () => {
+    it("returns 201 and authorization token when user details are valid", async () => {
+      const user = createValidUserDetail();
+      const response = await httpRequest.post("/auth/signup", user);
+
+      expect(response.status).toBe(201);
+      expect(response.data.token.length).toBeGreaterThan(0);
+    });
+
+    it("returns 409 when username is already registered", async () => {
+      const user = createValidUserDetail();
+      let response = await httpRequest.post("/auth/signup", user);
+      expect(response.status).toBe(201);
+
+      response = await httpRequest.post("/auth/signup", user);
+
+      expect(response.status).toBe(409);
+      expect(response.data.message).toBe("user already registered");
+    });
+
+    test.each([
+      { missingFieldName: "name", expectedMessage: "name is missing" },
+      {
+        missingFieldName: "username",
+        expectedMessage: "Invalid value",
+      },
+      { missingFieldName: "email", expectedMessage: "invalid email" },
+      {
+        missingFieldName: "password",
+        expectedMessage: "password should be at least 5 characters",
+      },
+    ])(
+      "returns 400 when $missingFieldName field is missing",
+      async ({ missingFieldName, expectedMessage }) => {
+        const user = createValidUserDetail();
+        delete user[missingFieldName];
+        const response = await httpRequest.post("/auth/signup", user);
+
+        expect(response.status).toBe(400);
+        expect(response.data.message).toBe(expectedMessage);
+      }
+    );
+
+    it("returns 400 when password is too short", async () => {
+      const user = { ...createValidUserDetail(), password: "123" };
+
+      const response = await httpRequest.post("/auth/signup", user);
+
+      expect(response.status).toBe(400);
+      expect(response.data.message).toBe(
+        "password should be at least 5 characters"
+      );
+    });
+  });
+
+  describe("POST to /auth/login", () => {
+    it("returns 200 status when logged in successfully", async () => {
+      const newUser = await createNewUser()
+      const response = await httpRequest.post('/auth/login', {username: newUser.username, password: newUser.password})
+
+      expect(response.status).toBe(200)
       expect(response.data.token.length).toBeGreaterThan(0)
-    })
+    });
 
-    it('returns 409 when username is already registered', async () => {
-      const user = createValidUserDetail()
-      let response = await httpRequest.post('/auth/signup', user)
-      expect(response.status).toBe(201)
+    test.each([
+      {inputData: 'username', expectedMessage: 'login failed'},
+      {inputData: 'password', expectedMessage: 'login failed'}
+    ])('returns 401 status when login is failed with wrong $wrongData', async ({inputData, expectedMessage}) => {
+      const newUser = await createNewUser()
+      newUser[inputData] = newUser[inputData].toUpperCase().concat('test')
+      const response = await httpRequest.post('/auth/login', {username: newUser.username, password: newUser.password})
 
-      response = await httpRequest.post('/auth/signup', user)
-
-      expect(response.status).toBe(409)
-      expect(response.data.message).toBe('user already registered')
+      expect(response.status).toBe(401)
+      expect(response.data.message).toBe(expectedMessage)
     })
 
     test.each([
-      {missingFieldName: 'name', expectedMessage:'name is missing'},
-      {missingFieldName:'username', expectedMessage:'username should be at least 5 characters'},
-      {missingFieldName:'email', expectedMessage: 'invalid email'},
-      {missingFieldName:'password', expectedMessage:'password should be at least 5 characters'},
-    ])('returns 400 when $missingFieldName field is missing', async ({missingFieldName, expectedMessage})=> {
-      const user = createValidUserDetail()
-      delete user[missingFieldName]
-      const response = await httpRequest.post('/auth/signup', user)
+      {inputData: 'username', expectedMessage:'username should be at least 5 characters'},
+      {inputData: 'password', expectedMessage: 'password should be at least 5 characters'}
+    ])('returns 400 when validation is failed due to $inputData', async ({inputData, expectedMessage}) => {
+      const newUser = await createNewUser()
+      newUser[inputData] = 'ab'
+      const response = await httpRequest.post('/auth/login', {username: newUser.username, password: newUser.password})
 
       expect(response.status).toBe(400)
       expect(response.data.message).toBe(expectedMessage)
     })
 
-    it('returns 400 when password is too short', async () => {
-      const user = {...createValidUserDetail(), password: '123'}
+  });
 
-      const response = await httpRequest.post('/auth/signup', user)
+  describe('GET to /auth/me', () => {
+    it('returns 200 status when token in Authorization header is valid', async () => {
+      const userRegistered = await createNewUser()
+      const response = await httpRequest.get('/auth/me', {
+        headers: {Authorization: `Bearer ${userRegistered.jwt}`}
+      })
 
-      expect(response.status).toBe(400)
-      expect(response.data.message).toBe('password should be at least 5 characters')
-
+      expect(response.status).toBe(200)
+      expect(response.data).toMatchObject({username: userRegistered.username, userId: userRegistered.userId, token: userRegistered.jwt})
     })
 
+    it('returns 404 status when token in Authorization header is invalid', async () => {
+      const userRegistered = await createNewUser()
+      userRegistered.jwt = undefined
+      const response = await httpRequest.get('/auth/me', {
+        headers: {Authorization: `Bearer ${userRegistered.jwt}`}
+      })
+
+      expect(response.status).toBe(401)
+      expect(response.data.message).toBe('Authentication Error1')
+    })
   })
-})
 
-function createValidUserDetail () {
-  const fakeUser = faker.helpers.userCard()
-  const {name, username, email} = fakeUser
-  const password = faker.internet.password(10, true)
-  const user = {name, username, email, password}
+  async function createNewUser() {
+    const userDetail = createValidUserDetail();
+    const userRegistered = await httpRequest.post("/auth/signup", userDetail);
+    return {
+      ...userDetail,
+      userId: userRegistered.data.userId,
+      jwt: userRegistered.data.token,
+    };
+  }
+});
 
-  return user
+function createValidUserDetail() {
+  const fakeUser = faker.helpers.userCard();
+  const { name, username, email } = fakeUser;
+  const password = faker.internet.password(10, true);
+  const user = { name, username, email, password };
+
+  return user;
 }
